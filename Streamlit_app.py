66f5f5e4c8cde71b3a49b2908e0fade0e381bab4
Senthil Kumaran <132420106+Senthilsk10@users.noreply.github.com>
2023-05-06 22:53:51 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,7 +4,7 @@ openai.api_key = OPENAI_API_KEY
 st.write("this is my first streamlit app")
 prompt = "how are you doing"
 response = openai.Completion.create(
-  engine='text-davinci-curie',prompt = prompt
+  engine='text-davinci-curie',prompt = prompt,
   max_tokens=10)
 
 completion_text = response.choices[0].text.strip()
